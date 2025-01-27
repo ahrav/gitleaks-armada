c8d2f9fed826301a71f1bbab509cd85971f1e0f6
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2025-01-27 08:48:59 -0800

    don't use key when publishing takss

--- internal/app/orchestration/orchestrator.go
+++ internal/app/orchestration/orchestrator.go
@@ -514,7 +514,6 @@ func (o *Orchestrator) startFreshEnumerations(ctx context.Context, cfg *config.C
 					if err := o.eventPublisher.PublishDomainEvent(
 						taskCtx,
 						enumeration.NewTaskCreatedEvent(job.JobID(), task),
-						events.WithKey(job.JobID().String()),
 					); err != nil {
 						taskSpan.RecordError(err)
 						taskSpan.SetStatus(codes.Error, "failed to publish task event")
