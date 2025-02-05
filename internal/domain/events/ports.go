0b5b35ca35c08e54acb142cc34394bb160b6853d
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2025-02-05 09:21:44 -0800

    add interface for Position Commiter

--- internal/domain/events/ports.go
+++ internal/domain/events/ports.go
@@ -41,6 +41,17 @@ type PositionTranslator interface {
 	ToStreamPosition(metadata PositionMetadata) (StreamPosition, error)
 }
 
+// OffsetCommitter is used to commit the position of the last event processed.
+// This is used to track the position of the last event processed so that we
+// can replay events from the same position.
+// This is useful for recovering from a system failure or for resuming from a
+// specific point in the event stream.
+type OffsetCommitter interface {
+	// CommitPosition signals that we've successfully processed
+	// everything up to 'pos' in the domain stream.
+	CommitPosition(ctx context.Context, pos DomainPosition) error
+}
+
 // DomainEventReplayer enables replaying domain events from a specific position in the event stream.
 type DomainEventReplayer interface {
 	// ReplayFromPosition replays domain events from a specific position in the event stream.
