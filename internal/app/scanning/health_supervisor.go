34f2a76a0b3245a740f902622524e05ec820ef7b
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2025-02-25 18:28:15 -0800

    move todo

--- internal/app/scanning/health_supervisor.go
+++ internal/app/scanning/health_supervisor.go
@@ -197,6 +197,8 @@ func (h *taskHealthSupervisor) checkForStaleTasks(ctx context.Context) {
 		attribute.String("cutoff_time", cutoff.Format(time.RFC3339)),
 	)
 
+	// TODO: We need to make sure that we don't mark a task stale if the job is pausing/paused.
+	// This could happen if a scanner gets killed prior to pausing its tasks related to a job.
 	staleTasks, err := h.healthSvc.FindStaleTasks(ctx, h.controllerID, cutoff)
 	if err != nil {
 		logr.Error(ctx, "Stale task detection failed",
@@ -222,8 +224,6 @@ func (h *taskHealthSupervisor) checkForStaleTasks(ctx context.Context) {
 		)
 
 		staleEvt := scanning.NewTaskStaleEvent(t.JobID(), t.TaskID(), scanning.StallReasonNoProgress, now)
-		// TODO: We need to make sure that we don't mark a task stale if the job is pausing/paused.
-		// This could happen if a scanner gets killed prior to pausing its tasks related to a job.
 		if err := h.stateHandler.HandleTaskStale(ctx, staleEvt); err != nil {
 			ctxLogr.Error(ctx, "Task state transition to stale failed",
 				"stall_reason", scanning.StallReasonNoProgress,
